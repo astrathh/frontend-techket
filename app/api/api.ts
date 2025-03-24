5b38c8819bac8004a327f029c41b30fabcba5e2a
astrathh <astratnho@hotmail.com>
2025-03-24 17:54:22 -0300

    remove componente de carregamento e atualiza URL da API para o ambiente de produção

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,3 +1,3 @@
 // frontend/src/config/api.ts
 // Definição da URL base da API
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://backend-techket.vercel.app/';
